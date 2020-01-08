585f9a50eaf266c7d4105eb277d10f70276fd802
locky5 <daikevin19@yahoo.com>
2020-01-08 02:43:15 -0500

    done

---
+++ queue.js
@@ -0,0 +1,36 @@
+var MyQueue = function() {
+    this.arr = []
+};
+
+/**
+ * Push element x to the back of queue.
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.arr.push(x)
+};
+
+/**
+ * Removes the element from in front of queue and returns that element.
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    return this.arr.shift()
+};
+
+/**
+ * Get the front element.
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    return this.arr[0]
+};
+
+/**
+ * Returns whether the queue is empty.
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return (this.arr.length === 0)
+};
